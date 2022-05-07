170e3841ef280c5e256f9cfc5c2666916893093f
serapioMartinez <luisserapiomartinez@gmail.com>
2022-05-06 23:07:53 -0500

    Otro commit para heroku

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 const env = process.env;
-
+const fs = require('fs')
 const config = {
     db:{
-        host: env.DB_HOST || 'localhost',
-        user: env.DB_USER || 'root',
-        password: env.DB_PASSWORD || '',
+        host: env.DB_HOST || 'turismappitt.mysql.database.azure.com',
+        user: env.DB_USER || "tur_appitt2022@turismappitt",
+        password: env.DB_PASSWORD || 'appItt_2022',
         database: env.DB_NAME || 'turismapp',
+        ssl: {
+            ca:fs.readFileSync(__dirname+'/BaltimoreCyberTrustRoot.crt.pem')
+        }
     }
 };
 
